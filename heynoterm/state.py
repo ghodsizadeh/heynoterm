55fda098550d158b708533076988fddc616aa96c
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2023-12-25 22:45:26 +0100

    Add state.py and test_state.py files

---
+++ heynoterm/state.py
@@ -0,0 +1,84 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+from pathlib import Path
+import json
+from enum import Enum
+
+class Language(Enum):
+
+    PYTHON = 'python'
+    MARKDOWN = 'markdown'
+    MATH = 'math'
+
+
+@dataclass
+class Block:
+    text: str
+    language: Language
+
+
+@dataclass
+class AppState:
+    blocks: List[Block] = field(default_factory=list)
+
+    def to_json(self) -> str:
+        return json.dumps({"blocks": [block.__dict__ for block in self.blocks]}, indent=4)
+
+    @staticmethod
+    def from_json(data: str) -> 'AppState':
+        json_data = json.loads(data)
+        blocks = [Block(**block) for block in json_data.get('blocks', [])]
+        return AppState(blocks=blocks)
+
+
+
+class DataManager:
+    """ 
+    A class to manage the data of the app.
+    To load, save and update the data.
+    """
+    
+    def __init__(self, path: Optional[Path] = None) -> None:
+        '''
+        Initialize the data manager.
+        With a default path for all OS.
+        to save the data.
+        '''
+        if path is None:
+            self.path = Path.home() / '.heynoterm.json'
+        else:
+            self.path = path            
+        self.state = AppState()
+
+    def load(self) -> None:
+        """Load the data from the path."""
+        if self.path.exists():
+            with open(self.path, 'r') as f:
+                self.state = AppState.from_json(f.read())
+        else:
+            self.save()
+    
+    def save(self) -> None:
+        """Save the data to the path."""
+        with open(self.path, 'w') as f:
+            f.write(self.state.to_json())
+    
+    def add_block(self, block: Block, index: Optional[int] = None) -> None:
+        """Add a block to the state."""
+        if index is not None:
+            self.state.blocks.insert(index, block)
+        else:
+            self.state.blocks.append(block)
+        self.save()
+    
+    def remove_block(self, index: int) -> None:
+        """Remove a block from the state."""
+        self.state.blocks.pop(index)
+        self.save()
+    
+    def update_block(self, index: int, block: Block) -> None:
+        """Update a block in the state."""
+        self.state.blocks[index] = block
+        self.save()
+    
+
---
+++ tests/test_state.py
@@ -0,0 +1,87 @@
+import json
+from pathlib import Path
+import pytest
+from heynoterm.state import Block, AppState, DataManager
+
+@pytest.fixture(scope="function")
+def data_path() -> Path:
+    # create a data.json file and delete it after the test
+    path = Path(__file__).parent / "data.json"
+    yield path
+    path.unlink(missing_ok=True)
+
+
+@pytest.fixture(scope="function")
+def data_manager(data_path) -> DataManager:
+    return DataManager(path=data_path)
+
+
+
+
+
+def test_block_to_json(data_manager):
+    block = Block(text="Hello", language="python")
+    assert block.text == "Hello"
+    assert block.language == "python"
+
+    
+
+
+def test_app_state_to_json():
+    app_state = AppState(blocks=[Block(text="Hello", language="python")])
+    # convert to json
+    json_data = app_state.to_json()
+    # expected json
+    expected_dict = {"blocks": [{"text": "Hello", "language": "python"}]}
+    # convert expected json to string
+    expected_json = json.dumps(expected_dict, indent=4)
+    # assert json data is equal to expected json
+    assert json_data == expected_json
+    
+    
+
+def test_app_state_from_json():
+    app_state = AppState.from_json('{"blocks": [{"text": "Hello", "language": "python"}]}')
+    assert len(app_state.blocks) == 1
+    assert app_state.blocks[0].text == "Hello"
+    assert app_state.blocks[0].language == "python"
+
+
+def test_data_manager_load(data_manager: DataManager, data_path: Path):
+    data_path.write_text('{"blocks": [{"text": "Hello", "language": "python"}]}')
+    data_manager.load()
+    state = data_manager.state
+    assert len(state.blocks) == 1
+    assert state.blocks[0].text == "Hello"
+    assert state.blocks[0].language == "python"
+
+
+def test_data_manager_save(data_manager: DataManager, data_path: Path):
+    data_manager.add_block(Block(text="Hello", language="python"))
+    data_manager.save()
+    assert data_path.exists()
+    assert json.loads(data_path.read_text()) == {"blocks": [{"text": "Hello", "language": "python"}]}
+
+
+def test_data_manager_add_block(data_manager: DataManager):
+    block = Block(text="Hello", language="python")
+    data_manager.add_block(block)
+    assert len(data_manager.state.blocks) == 1
+    assert data_manager.state.blocks[0] == block
+
+
+def test_data_manager_remove_block(data_manager: DataManager):
+    
+    block = Block(text="Hello", language="python")
+    data_manager.add_block(block)
+    data_manager.remove_block(0)
+    assert len(data_manager.state.blocks) == 0
+
+
+def test_data_manager_update_block(data_manager: DataManager):
+    block = Block(text="Hello", language="python")
+    data_manager.add_block(block)
+    updated_block = Block(text="Hi", language="python")
+    data_manager.update_block(0, updated_block)
+    assert len(data_manager.state.blocks) == 1
+    assert data_manager.state.blocks[0] == updated_block
